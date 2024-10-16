9d3798834daa5af9c0d429f6896fd5c4ffd03986
Julia <j.e.egorova@gmail.com>
2024-10-16 12:13:13 +0300

    Task 1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,7 +9,20 @@ document.addEventListener("turbo:before-render", (event) => {
   prevPath = window.location.pathname;
   event.detail.render = async (prevEl, newEl) => {
     await new Promise((resolve) => setTimeout(() => resolve(), 0));
-    morphdom(prevEl, newEl);
+
+    morphdom(prevEl, newEl, {
+      onBeforeElUpdated: function(fromEl, toEl) {
+        if (fromEl.hasAttribute("data-turbo-morph-permanent")) {
+          return false;
+        }
+
+        if (fromEl.isEqualNode(toEl)) {
+          return false;
+        }
+
+        return true;
+      },
+    });
   };
 
   if (document.startViewTransition) {
