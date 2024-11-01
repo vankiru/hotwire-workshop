d07f74c80503bff3f88eb841ed2e5d7c46d2d8b7
Julia <j.e.egorova@gmail.com>
2024-11-01 16:30:41 +0300

    Fix player re-rendering when Turbo renders the cache

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,16 +7,49 @@ let prevPath = window.location.pathname;
 document.addEventListener("turbo:before-render", (event) => {
   Turbo.navigator.currentVisit.scrolled = prevPath === window.location.pathname;
   prevPath = window.location.pathname;
+
   event.detail.render = async (prevEl, newEl) => {
     await new Promise((resolve) => setTimeout(() => resolve(), 0));
 
     morphdom(prevEl, newEl, {
+      onBeforeNodeAdded: function(node) {
+        // Do not add/discard permanent nodes when the page is
+        // rendered from the cache, as the cache may contain a
+        // stale version of the node.
+        //
+        // For example, when we start to play a new track, the
+        // cache will contain the previous one. So, Turbo will
+        // remove the current track and replace it with the
+        // previous one while rendering the cached version.
+        // Then, it will swap the tracks again when it renders
+        // the actual verion of the page.
+        if (
+          node && node.hasAttribute &&
+          node.hasAttribute("data-turbo-morph-permanent") &&
+          document.documentElement.hasAttribute("data-turbo-preview")
+        ) {
+          return false;
+        }
+
+        return node;
+      },
       onBeforeElUpdated: function(fromEl, toEl) {
         if (fromEl.hasAttribute("data-turbo-morph-permanent")) {
           return false;
         }
 
         if (fromEl.isEqualNode(toEl)) {
+          return false;
+        }
+
+        return true;
+      },
+      onBeforeNodeDiscarded: function(node) {
+        if (
+          node && node.hasAttribute &&
+          node.hasAttribute("data-turbo-morph-permanent") &&
+          document.documentElement.hasAttribute("data-turbo-preview")
+        ) {
           return false;
         }
 
